2578aad0243028dca212800bd242d0cc6d7253f7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 12:15:18 -0800

    returning a list needs fixing

--- wordcount.py
+++ wordcount.py
@@ -7,7 +7,7 @@ def word_count(filename):
     for line in open_file:
         words = line.split()
         for word in words:
-            counted_words[word] = counted_words.get(word, 0) + 1
+            print counted_words[word] = counted_words.get(word, 0) + 1
     # print "%s: %d" % (word, counted_words)
     return counted_words
 
