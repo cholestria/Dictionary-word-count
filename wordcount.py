e76069914d8d1717513b81c8e727d7282ae0d373
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 12:21:29 -0800

    working as intended

--- wordcount.py
+++ wordcount.py
@@ -7,9 +7,10 @@ def word_count(filename):
     for line in open_file:
         words = line.split()
         for word in words:
-            print counted_words[word] = counted_words.get(word, 0) + 1
-    # print "%s: %d" % (word, counted_words)
-    return counted_words
+            counted_words[word] = counted_words.get(word, 0) + 1
+    for word, number in counted_words.items():
+        print "%s: %s" % (word, number)
+    # return counted_words
 
 
-print word_count("test.txt")
+word_count("test.txt")
